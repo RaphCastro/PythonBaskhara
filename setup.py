4d11e9ae3fd9f4fe44523449596f38188af6681a
Castro <raphaelcastro1202@gmail.com>
2020-02-17 23:21:06 -0300

    iniciando o arquivo EXE

---
+++ setup.py
@@ -0,0 +1,29 @@
+import sys
+from cx_Freeze import setup, Executable
+
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+executables = [
+        Executable("OfertaDem.py", base=base)
+]
+
+buildOptions = dict(
+        packages = [],
+        includes = ["math"],
+        include_files = [],
+        excludes = []
+)
+
+
+
+
+setup(
+    name = "Funcoes_Economicas",
+    version = "1.0",
+    description = "O programa executa funcoes simples de economia",
+    options = dict(build_exe = buildOptions),
+    executables = executables
+ )
